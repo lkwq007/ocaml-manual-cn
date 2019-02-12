0e06d2ed8130875c5e4f0773b309371ce8b53fce
Lnyan <lkwq007@gmail.com>
2019-02-12 18:00:34 +0800

    Update travis ci & makefile

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ OPAM_DEPENDS="hevea"
 
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam texlive-full
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
