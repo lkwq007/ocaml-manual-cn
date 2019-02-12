293e4cd478270228dd7d0404dc35364ae34cca07
Lnyan <lkwq007@gmail.com>
2019-02-12 17:40:31 +0800

    Add travis ci

---
+++ .travis-ci.sh
@@ -0,0 +1,19 @@
+# inspired by https://gist.github.com/avsm/6757425
+
+OPAM_DEPENDS="hevea"
+
+echo "yes" | sudo add-apt-repository ppa:avsm/ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+opam init 
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+make
