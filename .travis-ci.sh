d52cd89bc64a542a4db9ca91533e8fe2dea83ea1
Lnyan <lkwq007@gmail.com>
2019-02-12 22:30:59 +0800

    Update travis ci

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,6 +3,7 @@
 OPAM_DEPENDS="hevea"
 
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa
+echo "yes" | sudo add-apt-repository ppa:jonathonf/texlive
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam texlive-full
 export OPAMYES=1
